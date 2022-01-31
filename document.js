bc1bdd0f138e0f1f05292623ed34eae64505c304
Olafr9500 <olafr.af.munso@gmail.com>
2022-01-31 11:08:06 +0100

    Fussion into one file

--- document.js
+++
@@ -1,4 +0,0 @@
-function docId(selector) { return document.getElementById(selector) };
-function docQ(selector) { return document.querySelector(selector) };
-function docQAll(selector) { return document.querySelectorAll(selector) };
-function docCrea(selector) { return document.createElement(selector) };
