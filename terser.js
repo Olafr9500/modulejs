48b9d02f57c41af0ac62ccd198d1f6f0a7d63009
Olafr9500 <olafr.af.munso@gmail.com>
2022-04-29 13:11:22 +0200

    Add script minify

---
+++ terser.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const { minify } = require('terser');
+// pour chaque fichier, on le minifie et on le stocke dans js avec le même nom + .min.js
+fs.readdirSync('.').forEach(file => {
+    if (file != 'rollup.config.js' && file != 'terser.js') {
+        if (file.endsWith('.js') && !file.endsWith('.min.js')) {
+            fs.writeFileSync(`./${file.replace('.js', '.min.js')}`, minify(fs.readFileSync(`./${file}`, 'utf8'), { compress: true, mangle: true }).code);
+            console.log(`${file} minify`);
+        }
+    }
+});
