b0b5ecdeb69ea8d1138a94f030cbcda03dc8430b
Olafr9500 <olafr.af.munso@gmail.com>
2022-05-06 10:06:04 +0200

    Simplification rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,18 @@
 const rollupConfig = [{
     input: 'build/index.js',
-    output: {
+    output: [{
         name: 'moduleJS',
         file: 'index.js',
         format: 'iife'
-    }
-},{
-    input: 'build/index.js',
-    output: {
+    }, {
         name: 'moduleJS',
         file: 'index.esm.js',
         format: 'es'
-    }
-},{
-    input: 'build/index.js',
-    output: {
+    }, {
         name: 'moduleJS',
         file: 'index.cjs.js',
         format: 'cjs'
-    }
+    }]
 }];
 
 module.exports = rollupConfig;
